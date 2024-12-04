2de6fa6662b9cc07b6261211e73416025851a6bc
dikaproject <dikagilang2007@gmail.com>
2024-12-04 23:34:37 +0700

    finishing project

--- next.config.js
+++ next.config.js
@@ -1,29 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: 'localhost',
-				port: '5000',
-				pathname: '/api/uploads/**',
-			},
-			{
-				protocol: 'https',
-				hostname: '*.ngrok-free.app',
-				port: '',
-				pathname: '/api/uploads/**',
-			}
-		],
-	},
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-			},
-		];
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/api/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.ngrok-free.app',
+        port: '',
+        pathname: '/api/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wasteless.intechsosmed.my.id',
+        port: '',
+        pathname: '/api/uploads/**',
+      }
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+          : 'http://localhost:5000/:path*', // Fallback URL
+      },
+    ];
+  },
 }
 
-module.exports = nextConfig
+export default nextConfig;
